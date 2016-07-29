32c4f65b49e2cde613ce6eadca3738f9f7b4a53b
alieeeee <alie960707@gmail.com>
2016-07-29 11:59:02 -0400

    try something

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 1337;
 
+var order = {};
+var restaurant = null;
+
+var slack_token = 'DtVFF8hPgNAAj70gex4tBhRk';
+
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,18 +16,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
 
 app.listen(port, function () {
-  console.log('Listening on port ' + port);
+    console.log('Listening on port ' + port);
 });
 
-app.post('/hello', function (req, res, next) {
+app.post('/order', function (req, res, next) {
+
+  if (! req.query.token || req.query.token.valueOf() != slack_token.valueOf()) {
+      console.log('Illegal request!');
+      return res.sendStatus(403)
+  }
+
+  var text = req.query.text;
+
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
-  };
-  // Loop otherwise..
+      "response_type": "in_channel",
+      "text": text,
+      "attachments": [
+          {
+              "text": text
+          }
+      ]
+  }
+
   if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
+      return res.status(200).json(botPayload);
   } else {
-    return res.status(200).end();
+      return res.status(200).end();
   }
 });
+
+var handle_request = function(text) {
+    
+}
