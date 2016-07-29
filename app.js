1ef16118baa3668558646a53aa4228d0747c1b94
alieeeee <alie960707@gmail.com>
2016-07-29 12:41:35 -0400

    try something

--- app.js
+++ app.js
@@ -20,26 +20,53 @@ app.listen(port, function () {
 });
 
 app.post('/order', function (req, res, next) {
+    if (! req.body.token || req.body.token.valueOf() != slack_token.valueOf()) {
+        console.log('Illegal request!');
+        return res.sendStatus(403);
+    }
 
-  if (! req.body.token || req.body.token.valueOf() != slack_token.valueOf()) {
-      console.log('Illegal request!');
-      return res.sendStatus(403)
-  }
+    var text = req.body.text;
 
-  var text = req.query.text;
+    var userName = req.body.user_name;
 
-  var userName = req.body.user_name;
-  var botPayload = {
-      "text": req.body.text,
-  }
+    handle_request(req.body.text, function(err, botPayload){
+        if(err) {
+            return res.status(200).json({ text: 'err: ' + err});
+        } else {
+            return res.status(200).json(botPayload);
+        }
+    })
+    /*var botPayload = {
+        "text": req.body.text,
+    }
 
-  if (userName !== 'slackbot') {
-      return res.status(200).json(botPayload);
-  } else {
-      return res.status(200).end();
-  }
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayload);
+    } else {
+        return res.status(200).end();
+    }*/
 });
 
-var handle_request = function(text) {
+var handle_request = function(text, callback) {
+    var set_restaurant_regex = /^set restaurant (.+)$/i;
+    var place_order_regex = /^order \[(.+)\](?: with \[(.+)\])?$/i;
+    var finished_order_regex = /^finished order$/i;
 
+    if(set_restaurant_regex.test(text)) {
+        restaurant = set_restaurant_regex.exec(text)[1];
+        return callback(null, {
+            "text": 'Restaurant set to ' + restaurant,
+        });
+    } else if(place_order_regex.test(text)) {
+        if(!restaurant) return callback('Restaurant is not set, you cant place order');
+        var order_item = place_order_regex.exec(text)[1];
+        return callback(null, {
+            "text": req.body.user_name + ' ordered' + oreder_item,
+        });
+    } else if(finished_order_regex.test(text)) {
+        restaurant = null;
+        return callback(null, {
+            "text": 'order finished!',
+        })
+    }
 }
